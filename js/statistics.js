f6ab8e05e97489498ac42b4761d558d2587f8b1f
kaktysshmanchik <kaktysshmanchik@gmail.com>
2026-09-25 11:32:48 +0300

    Apply new palette on statistics

--- js/statistics.js
+++ js/statistics.js
@@ -1,3 +1,4 @@
+(()=>{if(!document.querySelector('link[data-site-theme]')){const l=document.createElement('link');l.rel='stylesheet';l.href='css/theme.css';l.dataset.siteTheme='';document.head.appendChild(l);}})();
 (() => {
     const questions = document.querySelectorAll("[data-stat-question]");
 
